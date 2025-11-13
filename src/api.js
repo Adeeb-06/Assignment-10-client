ad672681ac5d28b5ba150476af0c2fa4e51047d9
Adeeb-06 <mohammadadeeb886@gmail.com>
2025-11-13 20:09:35 +0600

    url

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://assignment-10-server-xi-five.vercel.app/", // <-- change once here if URL changes
+  baseURL: "https://assignment-10-server-kohl-mu.vercel.app", // <-- change once here if URL changes
 });
 
 export default api;
